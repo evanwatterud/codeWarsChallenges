f54389b2c2d7967493d8bc006567e1d84fa35bbb
evanwatterud <cod.videos@hotmail.com>
2018-06-01 15:19:57 -0400

    first commit

---
+++ playingWithDigits.js
@@ -0,0 +1,29 @@
+/*
+Some numbers have funny properties. For example:
+
+  89 --> 8¹ + 9² = 89 * 1
+
+  695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+  46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+Given a positive integer n written as abcd... (a, b, c, d... being digits) and a
+positive integer p we want to find a positive integer k, if it exists, such as the
+sum of the digits of n taken to the successive powers of p is equal to k * n. In other words:
+
+  Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+  If it is the case we will return k, if not return -1.
+
+Note: n, p will always be given as strictly positive integers.
+*/
+
+// Solution
+function digPow(n, p){
+  var digits = n.toString().split('')
+  var total = digits.reduce(function(acc, currValue, currIndex) {
+    return currIndex == 1 ? Math.pow(Number(acc), p) + Math.pow(Number(currValue), p + currIndex) : acc + Math.pow(Number(currValue), p + currIndex)
+  })
+
+  return (total % n == 0) ? (total / n) : -1
+}
